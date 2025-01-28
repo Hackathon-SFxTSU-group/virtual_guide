18568baa60037c36c9649c4fa7efab96ff1f626b
Danila Krasnov <krasnov.d.a.20@mail.ru>
2025-01-28 13:42:17 +0300

    Изменил код для теста подключения

--- app/main.py
+++ app/main.py
@@ -9,31 +9,40 @@ from PIL import Image
 import io
 import os
 import uvicorn
+from pydantic import BaseModel
 
-from app.nn_models.Embed import QuestionAnsweringSystem
-from app.nn_models.ImageClassifier import ImageClassifier
+#from app.nn_models.Embed import QuestionAnsweringSystem
+#from app.nn_models.ImageClassifier import ImageClassifier
 
 # Список классов (ваши классы)
-class_names = [
-    "Антонио Канова. Амур и Психея",
-    "Афродита Венера Таврическая",
-    "Колыванская ваза",
-    "Леонардо да Винчи. Мадонна Литта",
-    "Микеланджело. Скорчившийся мальчик",
-    "Статуя Юпитера",
-    "Часы павлин",
-    "Часы-яйцо Ротшильда фирмы Фаберже",
-    "Эрот с луком"
-]
+# class_names = [
+#     "Антонио Канова. Амур и Психея",
+#     "Афродита Венера Таврическая",
+#     "Колыванская ваза",
+#     "Леонардо да Винчи. Мадонна Литта",
+#     "Микеланджело. Скорчившийся мальчик",
+#     "Статуя Юпитера",
+#     "Часы павлин",
+#     "Часы-яйцо Ротшильда фирмы Фаберже",
+#     "Эрот с луком"
+# ]
 
-# Загрузка модели
-qa_system = QuestionAnsweringSystem(
-        faiss_index_path="faiss_index"
-    )
-classifier = ImageClassifier(
-    model_path='saved_models/best_model_resnet.pth',
-    class_names=class_names
-)
+## Модель для входных данных
+# class PredictRequest(BaseModel):
+#     image_url: str
+
+# # Модель для входных данных
+# class AskRequest(BaseModel):
+#     question: str
+
+# # Загрузка модели
+# qa_system = QuestionAnsweringSystem(
+#         faiss_index_path="faiss_index"
+#     )
+# classifier = ImageClassifier(
+#     model_path='saved_models/best_model_resnet.pth',
+#     class_names=class_names
+# )
 
 # Создание FastAPI приложения
 app = FastAPI()
@@ -41,77 +50,76 @@ app = FastAPI()
 # Настройка Jinja2
 templates = Jinja2Templates(directory="app/templates")
 
-# Папка для сохранения загруженных изображений
-UPLOAD_DIR = "app/static"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
+## Папка для сохранения загруженных изображений
+# UPLOAD_DIR = "app/static"
+# os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-# Главная страница с формой загрузки
+# # Главная страница с формой загрузки
+# @app.get("/", response_class=HTMLResponse)
+# async def upload_form(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
+# Тестовая страница
 @app.get("/", response_class=HTMLResponse)
 async def upload_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    context = {
+        'request': request,
+        'name': 'Admin',
+    }
+    return templates.TemplateResponse("test.html", context)
 
-# Загрузка изображения
-@app.post("/upload")
-async def upload_image(file: UploadFile = File(...)):
-    try:
-        # Читаем изображение
-        image_data = await file.read()
-        image = Image.open(io.BytesIO(image_data))
+# # Загрузка изображения
+# @app.post("/upload")
+# async def upload_image(file: UploadFile = File(...)):
+#     try:
+#         # Читаем изображение
+#         image_data = await file.read()
+#         image = Image.open(io.BytesIO(image_data))
 
-        # Сохраняем изображение на сервере
-        filename = f"{uuid4()}.png"
-        image_path = os.path.join(UPLOAD_DIR, filename)
-        image.save(image_path)
+#         # Сохраняем изображение на сервере
+#         filename = f"{uuid4()}.png"
+#         image_path = os.path.join(UPLOAD_DIR, filename)
+#         image.save(image_path)
 
-        # Возвращаем информацию о загруженном изображении
-        return JSONResponse(content={
-            "filename": file.filename,
-            "format": image.format,
-            "size": {"width": image.width, "height": image.height},
-            "image_url": f"/static/{filename}"
-        })
+#         # Возвращаем информацию о загруженном изображении
+#         return JSONResponse(content={
+#             "filename": file.filename,
+#             "format": image.format,
+#             "size": {"width": image.width, "height": image.height},
+#             "image_url": f"/static/{filename}"
+#         })
 
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=400)
-
-from pydantic import BaseModel
-
-# Модель для входных данных
-class PredictRequest(BaseModel):
-    image_url: str
-
-# Предсказание
-@app.post("/predict")
-async def predict(request: PredictRequest):
-    try:
-        # Открываем изображение
-        file_path = request.image_url.lstrip("/")
-        image_path = os.path.join("app", file_path)
-
-        # Предсказание класса
-        predicted_class = classifier.predict(image_path)
-
-        # Возвращаем результат
-        return JSONResponse(content={"predicted_class": predicted_class})
-
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=400)
-    
-
-# Модель для входных данных
-class AskRequest(BaseModel):
-    question: str
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=400)
 
 
-@app.post("/ask")
-async def ask_question(request: AskRequest):
-    try:
-        # Ответ на вопрос
-        answer = qa_system.ask_question(request.question)
+# # Предсказание
+# @app.post("/predict")
+# async def predict(request: PredictRequest):
+#     try:
+#         # Открываем изображение
+#         file_path = request.image_url.lstrip("/")
+#         image_path = os.path.join("app", file_path)
 
-        # Возвращаем результат
-        return JSONResponse(content={
-            "answer": answer,
-        })
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=400)
+#         # Предсказание класса
+#         predicted_class = classifier.predict(image_path)
+
+#         # Возвращаем результат
+#         return JSONResponse(content={"predicted_class": predicted_class})
+
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=400)
+
+
+# @app.post("/ask")
+# async def ask_question(request: AskRequest):
+#     try:
+#         # Ответ на вопрос
+#         answer = qa_system.ask_question(request.question)
+
+#         # Возвращаем результат
+#         return JSONResponse(content={
+#             "answer": answer,
+#         })
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=400)
