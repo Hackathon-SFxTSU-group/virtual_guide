25335bca6f2166b7795ae8944a90f5a93b00c085
a.nizelskiy <a.nizelskiy@axelot.ru>
2025-02-05 21:29:43 +0300

    Обновить фронт

--- frontend-app/src/app/utils/utils.ts
+++ frontend-app/src/app/utils/utils.ts
@@ -1,8 +1,5 @@
 import {API_URL} from "../consts/consts";
 
-// TODO
-const api = 'http://45.151.62.93:8000';
-
 export function getUploadedImagePath(imageUrl: string) {
-  return `${api}${imageUrl}`;
+  return `${API_URL}${imageUrl}`;
 }
